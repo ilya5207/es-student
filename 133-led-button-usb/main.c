c8af5fb17b545b2e22dda1ab6b9d22db00d62fa6
Ilya Belogurov <ilya5207@mail.ru>
2026-09-17 19:22:49 +0200

    Сделана п1.3.4

--- 133-led-button-usb/main.c
+++ 133-led-button-usb/main.c
@@ -19,6 +19,26 @@ void set_led(bool on)
     printf("led %s\n", on ? "on" : "off");
 }
 
+bool handle_command(int command, bool led)
+{
+    if (command == 'e')
+    {
+        led = true;
+        set_led(led);
+    }
+    else if (command == 'd')
+    {
+        led = false;
+        set_led(led);
+    }
+    else
+    {
+        printf("unknown command: %c\n", command);
+    }
+
+    return led;
+}
+
 int main()
 {
     stdio_init_all();
@@ -44,5 +64,14 @@ int main()
         }
 
         previous = current;
-    }
+
+            int command = getchar_timeout_us(0);
+
+        if (command == PICO_ERROR_TIMEOUT)
+        {
+            continue;
+        }
+
+        led = handle_command(command, led);
+    }   
 } 
